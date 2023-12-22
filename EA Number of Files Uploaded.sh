d51a9af35bb7ffa554ca1a766da81d4858ea598b
zpropheter <128726098+zpropheter@users.noreply.github.com>
2023-12-21 23:43:45 -0700

    Update EA Number of Files Uploaded.sh

--- EA Number of Files Uploaded.sh
+++ EA Number of Files Uploaded.sh
@@ -2,7 +2,7 @@
 
 #Output a count of files uploaded from MacBook to remote administrator since installation
 
-filesUploadedFromClient=$(grep -R "virtual void grapplemdm::remo::fileTransfer::fromEndpoint::upload::Session::DoUpload(const SelectedFile::Ref) UploadComplete" /Users/Shared/jamfdata/ | awk '/UploadComplete/' | wc -l)
+filesUploadedFromClient=$(grep -ro "virtual void grapplemdm::remo::fileTransfer::fromEndpoint::upload::Session::DoUpload(const SelectedFile::Ref) UploadComplete" /Users/Shared/jamfdata/ | wc -l | xargs echo "")
 
 echo "<result>$filesUploadedFromClient</result>"
 
